3b4bf15f8b5f15c00c074188dabad9acb9e156b7
unknown <dnt1997@gmail.com>
2022-02-17 21:02:24 +0530

    pract leetcode

--- closure.js
+++ closure.js
@@ -1,14 +1,9 @@
 //A function, and a reference to that function's outer scope
-function outest(){
     function outer(){
         function inner(){
-            console.log(10)
+            return 10;
         }
-        let a = 10;
         return inner;
     }
-    return outer;
-}
-var a = outest();
-var b = a();
-b();
+var b = outer();
+console.log(b())
