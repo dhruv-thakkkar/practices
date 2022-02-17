b3828adf5775ccd75a614ddceb723c8943091aec
unknown <dnt1997@gmail.com>
2022-02-17 21:02:07 +0530

    pract leetcode

--- array/prac.js
+++ array/prac.js
@@ -1,32 +1,16 @@
-/* 
-Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
-Output: [0,4,1,3,2]
-Explanation:
-nums       index     target
-0            0        [0]
-1            1        [0,1]
-2            2        [0,1,2]
-3            2        [0,1,3,2]
-4            1        [0,4,1,3,2]
-*/
-/* let nums = [0,1,2,3,4];
-let index = [0,1,2,2,1];
-let usedIndex = [];
-let result = [];
-for(let i=0;i<nums.length;i++){
-    if(!usedIndex.includes(index[i])){
-        usedIndex.push(index[i])
-        result[index[i]] = nums[i];
-    }else{
-      result.splice(index[i],0,nums[i])
-    }
-}
-console.log(result) */
+let  word1  = ["abc", "d", "defg"], word2 = ["abcddefg"];
+let newWord1 = "";
+word1.forEach(element => {
+    newWord1 = newWord1+element;
+});
+let newWordArr1 = [newWord1];
 
-let nums = [0,1,2,3,4];
-let index = [0,1,2,2,1];
-let result = [];
-for(let i=0;i<nums.length;i++){
-    result.splice(index[i],0,nums[i])
-}
-console.log(result)
+let newWord2 = "";
+word2.forEach(element => {
+    newWord2 = newWord2+element;
+});
+let newWordArr2 = [newWord2];
+
+console.log(JSON.stringify(newWordArr1) === JSON.stringify(newWordArr2))
+
+    
