54defe8d5ef903599ddfb378a9dba743d9467d81
unknown <dnt1997@gmail.com>
2023-11-08 07:07:54 +0530

    practice ++

--- OOP/prototype.js
+++ OOP/prototype.js
@@ -4,6 +4,8 @@ const Person = function (name, age) {
 };
 
 const dhruv = new Person("dd", 1997);
+console.log(dhruv)
+
 Person.prototype.calAge = function () {
   console.log(this.age);
 };
--- callVsApply.js
+++ callVsApply.js
@@ -1,7 +1,33 @@
-var obj = {num : 2}
-var addToThis = function(a,b,c){
-    return this.num + a + b + c;
-}
-console.log(addToThis.call(obj,1,2,3))
-
-console.log(addToThis.apply(obj,[1,2,3]))
+let Lufthansa = {
+    airline: "Lufthansa",
+    code: "LH",
+    bookings: [],
+    book(flightNum, name) {
+      console.log(
+        `${name} booked a seat on ${this.airline}, flight ${flightNum} `
+      );
+      this.bookings.push({ flight: flightNum, name: name });
+    },
+  };
+  
+  Lufthansa.book(111, "Dhruv");
+  Lufthansa.book(222, "Raj");
+  console.log(Lufthansa);
+  
+  const euroWing = {
+    airline: "Eurowing",
+    code: "EW",
+    bookings: [],
+  };
+  
+  const book = Lufthansa.book;
+  //console.log(book(23,"Sd")) Normal function points THIS to undefined
+  
+  book.call(euroWing, 333, "Ram");
+  console.log(euroWing);
+  
+  book.apply(euroWing, [444, "Rahul"]); //APPLY USES ARRAY THATS ALL
+  
+  book.call(euroWing, ...[555, "Khush"]); //IF U DONT WANT TO USE APPLY USE CALL WITH SPREAD OPERATOR
+  console.log(euroWing);
+  
